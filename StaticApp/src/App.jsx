2949cf671fe78f79be0faf97ec4ba74d4d5e21b2
sbenhadjali <salma.benhadjali@veolia.com>
2024-10-29 11:13:21 +0100

    feat(static app): Add toogle for dark/light mode

--- StaticApp/src/App.jsx
+++ StaticApp/src/App.jsx
@@ -4,10 +4,19 @@ import Main from "./Components/Main"
 import Footer from "./Components/Footer.jsx";
 
 export default function App() {
+    const [darkMode, setDarkMode] = React.useState(window.matchMedia("(prefers-color-scheme: dark)").matches)
+
+    function toggleDarkMode() {
+        setDarkMode(prevMode => !prevMode)
+    }
+
     return (
         <div className="container">
-            <Navbar />
-            <Main />
+            <Navbar 
+                darkMode={darkMode} 
+                toggleDarkMode={toggleDarkMode}
+            />
+            <Main darkMode={darkMode}/>
             <Footer />
         </div>
     )
--- StaticApp/src/Components/Navbar.jsx
+++ StaticApp/src/Components/Navbar.jsx
@@ -1,11 +1,26 @@
 import React from "react"
 
-export default function Navbar() {
+export default function Navbar(props) {
     return (
-        <nav>
-            <img className="nav--icon" src="react-logo.svg" alt="logo" width="35.93" height="32"/>
+        <nav className={props.darkMode ? "dark" : ""}>
+            <img 
+                className="nav--icon" 
+                src="react-logo.svg" 
+                alt="logo" 
+                width="35.93" 
+                height="32"
+            />
             <h3 className="nav--logo_text">ReactFacts</h3>
-            <h4 className="nav--title">React Course - project 1</h4>
+            <div className="toogler">
+                <p className="toogler--light">Light</p>
+                    <div 
+                        className="toogler--slider"
+                        onClick={props.toggleDarkMode}
+                    >
+                        <div className="toogler--slider--circle"></div>
+                    </div>
+                <p className="toogler--dark">Dark</p>
+            </div>
         </nav>
     )
 }
